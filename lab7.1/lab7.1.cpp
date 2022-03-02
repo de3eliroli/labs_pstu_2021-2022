f184837e7a95765b74a44837f44a350a01facc18
Евгений Шеленков <91319891+de3eliroli@users.noreply.github.com>
2022-03-02 22:39:48 +0500

    Add files via upload

---
+++ lab7.1/lab7.1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>;
+#include<cmath>;
+#include <cstring>;
+using namespace std;
+int max_element(int n, int a[])
+{
+	int max = a[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+	}
+	return max;
+}
+
+size_t max_element(string str)
+{
+	int max = 0;
+	int k = 0;
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		k++;
+		if (str[i + 1] == ' ' || str[i + 1] == '\0')
+		{
+			if (k > max)
+			{
+				max = k;
+			}
+			k = -1;
+		}
+	}
+	return max;
+}
+
+
+int main()
+{
+	int a[] = { 1313,452432,125,111,29 };
+	cout << max_element("Hello fat user") << endl;
+	cout << max_element(5, a);
+}
